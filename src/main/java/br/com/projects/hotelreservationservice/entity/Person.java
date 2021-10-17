c6b54e5d57e95d480709afe76819a426774b337b
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2021-10-17 11:52:38 -0300

    Adding person entity.

---
+++ src/main/java/br/com/projects/hotelreservationservice/entity/Person.java
@@ -0,0 +1,154 @@
+package br.com.projects.hotelreservationservice.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Generic class to represent a person in the system.
+ * @author André
+ */
+@Entity
+@Table(name = "person")
+public class Person {
+    
+    /** Identifier */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_person", unique = true, nullable = false, insertable = true, updatable = true)
+    private Long id;
+
+    /** Person/company name */
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    /** Person/company telephone number */
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    /** Person/company email */
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @OneToOne(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+	@JoinColumn(name = "id_address", unique = false, nullable = true, insertable = true, updatable = true)
+    private Address address;
+
+
+
+    // ################ Constructors #################
+    public Person() {
+    }
+
+    public Person(String name, String phoneNumber, String email, Address address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    // ################ Getters/Setters #################
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [address=" + address + ", email=" + email + ", id=" + id + ", name=" + name + ", phoneNumber="
+                + phoneNumber + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (phoneNumber == null) {
+            if (other.phoneNumber != null)
+                return false;
+        } else if (!phoneNumber.equals(other.phoneNumber))
+            return false;
+        return true;
+    }
+    
+    
+}
