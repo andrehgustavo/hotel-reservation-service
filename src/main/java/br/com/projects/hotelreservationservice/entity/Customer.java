c8afdae099a58b4edee8915b2ccab56b6785480b
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2021-11-28 07:09:09 -0300

    Limpando imports não utilizados.

--- src/main/java/br/com/projects/hotelreservationservice/entity/Customer.java
+++ src/main/java/br/com/projects/hotelreservationservice/entity/Customer.java
@@ -1,11 +1,9 @@
 package br.com.projects.hotelreservationservice.entity;
 
 import java.io.Serializable;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
