1feca98e6a28055ac320838f569e48c9dddb9589
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2021-12-12 09:35:40 -0300

    Adicionando integração contínua.

---
+++ src/test/java/br/com/projects/hotelreservationservice/controllers/BookingControllerIntregalizationTests.java
@@ -0,0 +1,36 @@
+package br.com.projects.hotelreservationservice.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import br.com.projects.hotelreservationservice.entity.Booking;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BookingControllerIntregalizationTests {
+
+
+    @LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+
+
+    @Test 
+    void testGetAll() {
+		ResponseEntity<Booking[]> res = testRestTemplate.getForEntity("http://localhost:" + port + "/hotel-reservation/api/v1/bookings", Booking[].class);
+		
+		Booking[] bookings = res.getBody();
+		
+		assertEquals(16, bookings.length);
+	}
+
+}
